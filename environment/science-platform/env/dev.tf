f817062eba6df188f931fa08c5e9e4fcd1534d65
dspeck1 <dspeck@burwood.com>
2023-04-27 14:50:54 -0500

    add db creation

--- environment/science-platform/env/dev.tf
+++ environment/science-platform/env/dev.tf
@@ -1,2 +1,12 @@
+# S3
 bucket_name       = "test988989"
 environment_label = "dev"
+
+
+# Database
+db_name          = "rsp"
+disk_size        = 10
+database_type    = "postgres"
+database_version = "14.7"
+instance_class   = "db.t3.micro"
+username         = "rubin"
--- environment/science-platform/main.tf
+++ environment/science-platform/main.tf
@@ -4,3 +4,16 @@ module "bucket" {
   environment_label = var.environment_label
 
 }
+
+module "rsp-db" {
+  source           = "../../modules/database"
+  db_name          = var.db_name
+  disk_size        = var.disk_size
+  database_type    = var.database_type
+  database_version = var.database_version
+  instance_class   = var.instance_class
+  user_name        = var.user_name
+  password         = var.password
+}
+
+
